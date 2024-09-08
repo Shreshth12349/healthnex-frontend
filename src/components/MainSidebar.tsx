4f41fcea5a3e5a5c6e398965e617b7cf69fb5479
Lala Arnav Vatsal <lalaarnav003@gmail.com>
2024-09-09 01:58:11 +0530

    Updated Side bar.

--- src/components/MainSidebar.tsx
+++ src/components/MainSidebar.tsx
@@ -1,90 +1,74 @@
 "use client";
-import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-import {
-  CalendarIcon,
-  ChartBarIcon,
-  PencilSquareIcon,
-  QueueListIcon,
-  UserIcon,
-} from "@heroicons/react/24/outline";
-import { useState } from "react";
+import {Menu, MenuItem, Sidebar} from "react-pro-sidebar";
+import {CalendarIcon, ChartBarIcon, PencilSquareIcon, QueueListIcon, UserIcon,} from "@heroicons/react/24/outline";
+import {useState} from "react";
 import Link from "next/link";
 
 export default function MainSidebar() {
-  const [openSidebar, setOpenSidebar] = useState(false);
-  const [isOpen, setIsOpen] = useState(false);
+    const [openSidebar, setOpenSidebar] = useState(false);
 
-  return (
-    <Sidebar
-      collapsed={!openSidebar}
-      onMouseEnter={() => setOpenSidebar(true)}
-      onMouseLeave={() => setOpenSidebar(false)}
-      width={"200px"}
-      className="shadow h-screen fixed top-0 left-0 bg-opacity-100 bg-white text-black z-34"
-    >
-      <Link className="p-4" href={"/"}>
-        {openSidebar ? (
-          <h1 className="text-2xl font-bold transition text-center">
-            HealthNex
-          </h1>
-        ) : (
-          <h1 className="text-2xl font-bold transition text-center">HN</h1>
-        )}
-      </Link>
-      <Menu>
-        <MenuItem
-          title={"Dashboard"}
-          icon={<ChartBarIcon className="h-6 w-6 stroke-gray-600" />}
-          component={<Link href={"/dashboard"} />}
+    return (
+        <Sidebar
+            collapsed={!openSidebar}
+            onMouseEnter={() => setOpenSidebar(true)}
+            onMouseLeave={() => setOpenSidebar(false)}
+            width={"200px"}
+            className="shadow-lg h-screen fixed top-0 left-0 bg-white text-black z-50"
         >
-          Dashboard
-        </MenuItem>
-        <MenuItem
-          icon={<PencilSquareIcon className="h-6 w-6 stroke-gray-600" />}
-          component={<Link href={"/patients"} />}
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          Patients
-        </MenuItem>
-        {/*{openSidebar && isOpen && (*/}
-        {/*  <ul>*/}
-        {/*    <Link href={"/prescription/admin"}>*/}
-        {/*      <li className="flex justify-center hover:bg-gray-100 text-sm py-2 cursor-pointer">*/}
-        {/*        Admin Access*/}
-        {/*      </li>*/}
-        {/*    </Link>*/}
-        {/*    <Link href={"/prescription/user"}>*/}
-        {/*      <li className="flex justify-center hover:bg-gray-100 text-sm py-2 cursor-pointer">*/}
-        {/*        User Access*/}
-        {/*      </li>*/}
-        {/*    </Link>*/}
-        {/*  </ul>*/}
-        {/*)}*/}
-        <MenuItem
-          icon={<QueueListIcon className="h-6 w-6 stroke-gray-600" />}
-          component={<Link href={"/appointments"} />}
-        >
-          Appointments
-        </MenuItem>
-        <MenuItem
-          icon={<ChartBarIcon className="h-6 w-6 stroke-gray-600" />}
-          component={<Link href={"/appointments"} />}
-        >
-          Medical Knowledge Base
-        </MenuItem>
-        <MenuItem
-          icon={<CalendarIcon className="h-6 w-6 stroke-gray-600" />}
-          component={<Link href={"/schedule"} />}
-        >
-          Schedule
-        </MenuItem>
-        <MenuItem
-          icon={<UserIcon className="h-6 w-6 stroke-gray-600" />}
-          component={<Link href={"/profile"} />}
-        >
-          Profile
-        </MenuItem>
-      </Menu>
-    </Sidebar>
-  );
+            <Link className="p-4 flex items-center justify-center" href={"/"}>
+                <h1 className="text-2xl font-bold transition text-center">
+                    {openSidebar ? "HealthNex" : "HN"}
+                </h1>
+            </Link>
+            <Menu>
+                <MenuItem
+                    icon={<ChartBarIcon className="h-6 w-6 text-gray-600"/>}
+                    component={<Link href={"/dashboard"}/>}
+                    className="hover:bg-gray-100"
+                >
+                    Dashboard
+                </MenuItem>
+
+                <MenuItem
+                    icon={<PencilSquareIcon className="h-6 w-6 text-gray-600"/>}
+                    component={<Link href={"/patients"}/>}
+                    className="hover:bg-gray-100"
+                >
+                    Patients
+                </MenuItem>
+
+                <MenuItem
+                    icon={<QueueListIcon className="h-6 w-6 text-gray-600"/>}
+                    component={<Link href={"/appointments"}/>}
+                    className="hover:bg-gray-100"
+                >
+                    Appointments
+                </MenuItem>
+
+                <MenuItem
+                    icon={<ChartBarIcon className="h-6 w-6 text-gray-600"/>}
+                    component={<Link href={"/knowledge-base"}/>}
+                    className="hover:bg-gray-100"
+                >
+                    Medical Knowledge Base
+                </MenuItem>
+
+                <MenuItem
+                    icon={<CalendarIcon className="h-6 w-6 text-gray-600"/>}
+                    component={<Link href={"/schedule"}/>}
+                    className="hover:bg-gray-100"
+                >
+                    Schedule
+                </MenuItem>
+
+                <MenuItem
+                    icon={<UserIcon className="h-6 w-6 text-gray-600"/>}
+                    component={<Link href={"/profile"}/>}
+                    className="hover:bg-gray-100"
+                >
+                    Profile
+                </MenuItem>
+            </Menu>
+        </Sidebar>
+    );
 }
